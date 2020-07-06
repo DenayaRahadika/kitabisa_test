2c9985069d57dfc9355856478c609abb69fa8a24
DenayaRahadika <denayarahadika@gmail.com>
2020-07-06 10:53:32 -0700

    add no 4 main file and unittest

---
+++ no_4/combine_2_list.py
@@ -0,0 +1,37 @@
+'''
+Sort and combine these 2 lists and print the result
+Input 1: 4,3,6,5,1,2
+Input 2: F,C,D,B,A
+Output : [1:A],[2:B],[3:C],[4:D],[5:F],[6:NULL]
+Edited Output: (due to wrong output shape): [1,"A"],[2, "B"],[3, "C"],[4, "D"],[5, "F"],[6, "NULL"]
+'''
+
+class SortCombine(object):
+    def sort_combine(self, list1, list2):
+        list1 = sorted(list1)
+        list2 = sorted(list2)
+
+        n1 = len(list1)
+        n2 = len(list2)
+        output = []
+        i = 0
+        while n1 > 0 or n2 > 0:
+            if n1 <= 0:
+                output.append(["NULL", list2[i]])
+            elif n2 <= 0:
+                output.append([list1[i], "NULL"])
+            else:
+                output.append([list1[i], list2[i]])
+            i += 1
+            n1 -= 1
+            n2 -=1
+        return output
+
+
+if __name__ == "__main__":
+    sc = SortCombine()
+    list1 = []
+    list2 = []
+
+    output = sc.sort_combine(list1, list2)
+    print(output)
---
+++ no_4/n_prime.py
@@ -0,0 +1,35 @@
+'''
+Find first N prime number, and print the result
+Input: 4
+Output : 2, 3, 5, 7
+
+'''
+
+class PrimeBuilder(object):
+    def is_prime(self, x):
+        # if there is any number between 1 and x-1 that can divide x, then x is not prime
+        return not any(x//i == x/i for i in range(x-1, 1, -1))
+
+    def n_prime(self, n):
+        prime_arrays = []
+        x = 2
+        while len(prime_arrays) < n:
+            if self.is_prime(x):
+                prime_arrays.append(x)
+            x += 1
+        return prime_arrays
+
+
+if __name__ == "__main__":
+    pb = PrimeBuilder()
+    n = None
+    while n is None:
+        try:
+            n = int(input("How many prime numbers do you want to find?: "))
+        except ValueError:
+            print("Not an integer value...")
+    prime_arrays = pb.n_prime(n)
+
+    # The value will be printed and saved as array like [2, 3, 5, 7] not string like "2, 3, 5, 7" to make the unittest easier.
+    print(prime_arrays)
+
